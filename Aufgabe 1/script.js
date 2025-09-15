39bcbfe76b2420ddfbb88d62dfd4d7d58499acdf
Mary Habibi <mary.habibi@iqdigital.de>
2025-09-15 19:02:26 +0200

    Update video file source in JWPlayer setup for improved playback

--- Aufgabe 1/script.js
+++ Aufgabe 1/script.js
@@ -1,12 +1,12 @@
 jwplayer("player").setup({
     playlist: [{
-        file: "https://storage.googleapis.com/interactive-media-ads/media/android.mp4", // لینک ویدئو
-        image: "https://via.placeholder.com/640x360" // عکس پوستر (اختیاری)
+        file: "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+        image: "https://via.placeholder.com/640x360"
     }],
-    width: "100%", // عرض پلیر
-    height: "360px", // ارتفاع پلیر
-    autostart: false, // خودکار پخش نشه
-    mute: true, // اول بی‌صدا باشه
+    width: "100%",
+    height: "360px",
+    autostart: false,
+    mute: true,
     advertising: {
         client: "vast",
         tag: "https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dlinear&correlator="
