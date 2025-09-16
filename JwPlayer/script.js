22469fcf607692bcde3f4ce13d1d32e5d86a1d2f
Mary Habibi <mary.habibi@iqdigital.de>
2025-09-16 13:08:11 +0200

    Refactor JW Player setup by commenting out unused advertising configurations for clarity

--- JwPlayer/script.js
+++ JwPlayer/script.js
@@ -6,14 +6,17 @@ jwplayer("player").setup({
     width: "100%",
     height: "360px",
     autostart: false,
-    mute: true,
-    advertising: {
-        client: "vast",
-        tag: "https://pubads.g.doubleclick.net/gampad/ads?iu=/183/iqd_videoplayer/videoplayer&description_url=" + encodeURIComponent(window.location.href) + "&cust_params=pos%3Dpre%26tile%3D169%26kw%3Dmary_testplayer&npa=0&sz=16x9%7C480x360%7C640x360%7C640x480&pmnd=0&pmxd=32000&pmad=2&pod=1&vpos=preroll&plcmt=1&gdfp_req=1&output=vast&env=vp&unviewed_position_start=1&impl=s&correlator=" + Date.now()
+    // mute: true,
+    // advertising: {
+    //     client: "vast",
+    //     tag: "https://pubads.g.doubleclick.net/gampad/ads?iu=/183/iqd_videoplayer/videoplayer&description_url=" + encodeURIComponent(window.location.href) + "&cust_params=pos%3Dpre%26tile%3D169%26kw%3Dmary_testplayer&npa=0&sz=16x9%7C480x360%7C640x360%7C640x480&pmnd=0&pmxd=32000&pmad=2&pod=1&vpos=preroll&plcmt=1&gdfp_req=1&output=vast&env=vp&unviewed_position_start=1&impl=s&correlator=" + Date.now()
 
-        // `https://pubads.g.doubleclick.net/gampad/ads?sz=16x9%7C480x360%7C640x360%7C640x480&iu=/183,22337032985/iqdspiegel/videoplayer&cust_params=pos%3Dpre%26tile%3D169%26kw%3Diqadtile169%2C{customparam}%26player%3D{name}&pmnd=0&pmxd=32000&pmad=2&pod=1&vpos=preroll&plcmt=1&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&url=__referrer__&description_url=__page-url__&correlator=__timestamp__`
 
-    }
+
+    //midroll --->.  https://pubads.g.doubleclick.net/gampad/ads?sz=16x9%7C480x360%7C640x360%7C640x480&iu=/183/iqd_videoplayer/videoplayer&cust_params=pos%3Dmid%26tile%3D1692%26kw%3Diqadtile1692%2Cmary_testplayer%26&pmnd=0&pmxd=32000&pmad=2&pod=2&vpos=midroll&plcmt=1&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&url=__referrer__&description_url=__page-url__&correlator=__timestamp__"
+    //     // `https://pubads.g.doubleclick.net/gampad/ads?sz=16x9%7C480x360%7C640x360%7C640x480&iu=/183,22337032985/iqdspiegel/videoplayer&cust_params=pos%3Dpre%26tile%3D169%26kw%3Diqadtile169%2C{customparam}%26player%3D{name}&pmnd=0&pmxd=32000&pmad=2&pod=1&vpos=preroll&plcmt=1&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&url=__referrer__&description_url=__page-url__&correlator=__timestamp__`
+
+    // }
 });
 // Debug für Ad Errors
 jwplayer().on('adError', function (event) {
