2729cb9490b24b52a4aee0397eb86d87cd7d846b
Mary Habibi <mary.habibi@iqdigital.de>
2025-09-19 14:32:47 +0200

    Refactor video slot

--- ry-jw.js
+++ ry-jw.js
@@ -1,12 +1,16 @@
 (() => {
     window.relevantDigital = window.relevantDigital || {};
     relevantDigital.cmd = relevantDigital.cmd || [];
+
     relevantDigital.cmd.push(() => {
         relevantDigital.addPrebidConfig({ consentManagement: { cmpApi: 'none' } });
 
         const PRE_ROLL_ID = relevantDigital.defineVideoSlots([
-            { path: '/183/iqd_videoplayer/videoplayer', customParams: { pos: 'pre', tile: '169', kw: 'iqadtile169,mary_testplayer' } }
-        ]).map((s) => s.getSlotElementId());
+            {
+                path: '/183/iqd_videoplayer/videoplayer',
+                customParams: { pos: 'pre', tile: '169', kw: 'iqadtile169,mary_testplayer' }
+            }
+        ]).map(s => s.getSlotElementId());
         console.log("Pre-Roll Slot ID:", PRE_ROLL_ID);
 
         relevantDigital.loadPrebid({
@@ -14,11 +18,12 @@
             manageAdserver: true,
             collapseEmptyDivs: true,
             collapseBeforeAdFetch: false,
-            // allowedDivIds: [PRE_ROLL_ID, ...MID_ROLL_IDS], 
             noSlotReload: false
         });
 
-        relevantDigital.loadVideoUrls([PRE_ROLL_ID], (urls) => {
+        relevantDigital.loadVideoUrls(PRE_ROLL_ID, (urls) => {
+            console.log("Pre-Roll Ad Tag URL:", urls[0]);
+
             jwplayer("player").setup({
                 playlist: [{
                     file: "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
@@ -38,7 +43,7 @@
                 }
             });
 
-            jwplayer().on('adError', function (event) {
+            jwplayer("player").on('adError', (event) => {
                 console.log('Ad Error:', event.message, event.code);
             });
         });
